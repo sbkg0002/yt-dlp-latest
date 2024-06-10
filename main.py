f93ecde05749d9c3702c3a30c3e6d3de07424f53
Johan Steenhoven <Johan.Steenhoven@devolksbank.nl>
2024-06-10 21:07:42 +0200

    Ignore, input directory

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import scrapetube
 import yt_dlp
+import argparse
+
+# Create the parser
+parser = argparse.ArgumentParser(description='Download some YT channels.')
+
+# Add the optional flag for the directory
+parser.add_argument('-d', '--directory', type=str, help='The root directory to store videos.', default='.')
+args = parser.parse_args()
+directory = args.directory
 
 
 def dlvideo(channel: str, limit: int = 20) -> None:
-    videos = scrapetube.get_channel(channel_username=channel, limit=limit, sort_by="newest", content_type="videos")
+    videos = scrapetube.get_channel(channel_username=channel, limit=limit, sort_by='newest', content_type='videos')
     yt_opts = {
         'verbose': False,
         'format_sort': ['res:720', 'ext:mp4:m4a'],
-        "writethumbnail": False,
-        "outtmpl": "%(uploader)s/%(title)s.%(ext)s"
+        'writethumbnail': False,
+        'outtmpl': f'{directory}/%(uploader)s/%(title)s.%(ext)s'
     }
     ydl = yt_dlp.YoutubeDL(yt_opts)
 
     for video in videos:
-        vide_url = f"https://www.youtube.com/watch?v={video['videoId']}"
+        vide_url = f'https://www.youtube.com/watch?v={video["videoId"]}'
 
         print(f'Downloading: {vide_url}..')
         try:
@@ -25,19 +34,19 @@ def dlvideo(channel: str, limit: int = 20) -> None:
         else:
             yt_opts = {
                 'verbose': False,
-                "format": "best[ext=mp4]",
-                "writethumbnail": False,
-                "outtmpl": "%(uploader)s/%(title)s.%(ext)s"
+                'format': 'best[ext=mp4]',
+                'writethumbnail': False,
+                'outtmpl': f'{directory}/%(uploader)s/%(title)s.%(ext)s'
             }
             ydl.download(vide_url)
 
 
-# channel_list = ["BobdeBouwerOfficieel"]
-channel_list = ["jilloptv", "EOCheckpoint", "GameBawz"]
-# channel_list = ["EOCheckpoint"]
-# channel_list = ["deofficielepeppa"]
-# channel_list = ["BingNederlands"]
-# channel_list = ["MegaTrucks157"]
-# channel_list = ["Dutchtuber2", "WalnutMax"]
+# channel_list = ['BobdeBouwerOfficieel']
+# channel_list = ['jilloptv', 'EOCheckpoint', 'GameBawz']
+# channel_list = ['EOCheckpoint']
+# channel_list = ['deofficielepeppa']
+# channel_list = ['BingNederlands']
+# channel_list = ['MegaTrucks157']
+channel_list = ['HelperCars']
 for channel in channel_list:
     dlvideo(channel=channel, limit=40)
